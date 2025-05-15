d839417b01fd5de394424cee773711b8662e4a83
Recrusion <entertohacking@gmail.com>
2025-05-15 09:26:47 +0300

    add new models

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,7 +9,7 @@ type Users struct {
 }
 
 type SignUp struct {
-	Username string `json:db:"id"`
+	Username string `json:db:"username"`
 	Email    string `json:db:"email"`
 	Password string `json:"-"`
 	Address  string `json"db:"address"`
